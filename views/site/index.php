90699b1e7e47bd7af6f8b364e3b164b8a17e2e71
Vlad Usepusev <gerasimovv39@gmail.com>
2024-04-26 18:31:02 +0300

    Экзамен

---
+++ views/site/index.php
@@ -0,0 +1,141 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+<main>
+
+  <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel" data-bs-theme="light">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>Example headline.</h1>
+            <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Another example headline.</h1>
+            <p>Some representative placeholder content for the second slide of the carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <div class="container">
+          <div class="carousel-caption text-end">
+            <h1>One more for good measure.</h1>
+            <p>Some representative placeholder content for the third slide of this carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+  <!-- Marketing messaging and featurettes
+  ================================================== -->
+  <!-- Wrap the rest of the page in another container to center all the content. -->
+
+  <div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <h2 class="fw-normal">Отслеживаем</h2>
+        <p>Мы отслеживаем, в каких районах чаще всего происходит правонарушения и передаем сведения в ГИБДД.</p>
+        <p><a class="btn btn-secondary" href="#">View details »</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <h2 class="fw-normal">Следим</h2>
+        <p>Мы следим за вашей безопасностью днем и ночью, давая ответ на заявки в течении пяти-десяти минут после ее формирования.</p>
+        <p><a class="btn btn-secondary" href="#">View details »</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+        <h2 class="fw-normal">Контролируем</h2>
+        <p>Мы контролируем, чтобы каждое одобренное заявление было рассмотрено соответствующим органом.</p>
+        <p><a class="btn btn-secondary" href="#">View details »</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+
+
+    <!-- START THE FEATURETTES -->
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Портал <span class="text-body-secondary">“Нарушением нет”</span></h2>
+        <p class="lead">Портал “Нарушением нет” является передовой платформой по приему заявок о дорожных правонарушениях.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" height="70" xmlns="<div class="MMImageContainer"><img class="MMImage-Preview" src="//yandex-images.clstorage.net/1Mq003B21/f3e6e70vmgZ/t2VezY3ohSB3XCUC_vUW4xx_ZdJMsG5Y13cOk9_yTxXy1cYBYlhDGuMoBD_tCrB6S4tCmU6OCAqBlWNq9Dn8SGP0y19daEQiM85iBm4xFaBMvGBG3zF9jFX3DIeGaROL7s8YTGhECAVkaRJ_bRk3qliQTFzSSpcXwIjmaSpnLN0zItgEC7lI4P-JrkSxudpnCffl3R70slia7BoXiqxbCieB1YIjhMZCIWPQNiZb5y9WU0cEFseXFsxJ52GsLK3Q_6lXiMjyhWx7TqdDMLaTrgd4YZOV-D3fWGnW0Q2g2MjlgR4KJwcPS6nsnf4sE--t3tYAjdhPFBoKy-AsIq_j0bGq0grCcsqsMwcmQfg6Uq7L9-qR0fp5X9Ph2pBPr9iAIAWRBWhHjAKooBg-rhLkrxPeR4qTCp7XzEglp6OrKh42ZV7ARfADZzsD50e1tNNjSjDp0h-_PJ8UoxpQSmaQia3Bl0BjzkZHZugSsOAd6SBc2sWN24LZ1YUIqSzhIWxdOSxfQgB2TGN2i2FAt_dWJUe-p5Mbu7DYGCGV0QgnGEjlyp-EZI2HA-RkVf7uGS_tkB1NiRnCXBuMQ-rj7iliFHVhX0xHPoqrtYYvwD381-nMtGAenPP5V9wt0xXA7NpPKMAYS-MOBAeg7xT4bdeoL1WWhkRfwp0fDcjgYS0v5RlyYx5Kj3-OJTPCbwN5MZ9pTz4hHRQ2tNcX4JuZCGsfBS7GkUPnDANEouIUOCFWbyDRH0GPmoMVVI3KqWkn4CZc9qUSAIS_gi_5jOGEvnId70Xw7ttSO7lTlaSYkszsEghsy9XBYMGGjCCrEXShGqjqVZ1ChRMIkNOBwi1oou_lFPLlX00I_8rjuQmryv3x1uXDP2gTEfmzkR7s010KrBnFIMyegG_Pj4RqqJN9blRvY9PVCAnRAxIchcolqCnpoxCzrJwHiLvOqjUOrUyz9dOtjXRhFBK7-5vfqpIRh8" alt="Если на переходе горит светофор и инспектор гибдд также руководит - 81 фото" data-load-timing="1" role="presentation" style="max-height: 300px;"></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading fw-normal lh-1">Можно оставить заявления <span class="text-body-secondary">если не знаете куда обратиться</span></h2>
+        <p class="lead">Сознательные граждане со всей России оставляют здесь свои заявления, когда не знают, куда обратиться.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  height="70" xmlns="<div class="MMImageContainer"><img class="MMImage-Preview" src="//avatars.mds.yandex.net/i?id=b3186f9051dec5f3a403ffdc7eb5d2ff_l-4012127-images-thumbs&amp;n=13" alt="Когда сгорают штрафы ГИБДД по закону" data-load-timing="1" role="presentation" style="max-height: 300px;"></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">Обращение граждан <span class="text-body-secondary">доходит до</span></h2>
+        <p class="lead">Каждая обращение граждан доходит до государственной инспекции безопасности дорожного движения (ГИБДД) и проверяется на соответствия всем нормам и правил дорожного движения.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  height="70 " xmlns="<div class="MMImageContainer"><img class="MMImage-Preview" src="//avatars.mds.yandex.net/i?id=2d36a357d5e6514c74daa85a6384bbff_l-5880570-images-thumbs&amp;n=13" alt="10 способов понравиться инспекторам ГИБДД" data-load-timing="1" role="presentation" style="max-height: 300px;"></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
+
+
+  <!-- FOOTER -->
+  <footer class="container">
+    <p class="float-end"><a href="#">В начало страницы</a></p>
+    <p> +7 (999) 888 33-22
+    support@narusheniyam.net </p>
+  </footer>
+</main>
+<body style='background-color: rgb(199, 199, 209)'>
